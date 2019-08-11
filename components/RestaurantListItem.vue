8b1d7e3c359e5107e1c1b441e1f653f519366c03
yuk-tanaka <paradisecreek70@gmail.com>
2019-08-12 03:59:30 +0900

    閉業chip実装

--- components/RestaurantListItem.vue
+++ components/RestaurantListItem.vue
@@ -6,18 +6,20 @@
           <base-area-chip :area="restaurant.area"></base-area-chip>
         </v-flex>
         <v-flex xs12>
-          <v-chip
-            v-for="participant in restaurant.participants"
-            :key="participant.year"
-            color="blue lighten-4"
-            class="font-weight-medium mr-3"
-            label
-            small>
-            <span class="font-weight-medium">{{participant.year}}</span>
-          </v-chip>
+          <restaurant-list-item-year-chip :participants="restaurant.participants"></restaurant-list-item-year-chip>
         </v-flex>
         <v-flex xs12>
-          <h3 class="title">{{restaurant.name}}</h3>
+          <h3 class="title">
+            {{restaurant.name}}
+            <v-chip
+              v-if="restaurant.is_closed"
+              color="red lighten-4"
+              class="ml-3"
+              label
+              small>
+              <span class="font-weight-medium">閉業</span>
+            </v-chip>
+          </h3>
         </v-flex>
         <v-flex xs12>
           <base-restaurant-sns-link :restaurant="restaurant"></base-restaurant-sns-link>
@@ -34,12 +36,14 @@
 import BaseAreaChip from '~/components/BaseAreaChip.vue'
 import BaseRestaurantInformation from '~/components/BaseRestaurantInformation.vue'
 import BaseRestaurantSnsLink from '~/components/BaseRestaurantSnsLink.vue'
+import RestaurantListItemYearChip from '~/components/RestaurantListItemYearChip.vue'
 
 export default {
   components: {
     BaseAreaChip,
     BaseRestaurantInformation,
-    BaseRestaurantSnsLink
+    BaseRestaurantSnsLink,
+    RestaurantListItemYearChip
   },
   props: {
     restaurant: {type: Object, required: true}
---
+++ components/RestaurantListItemYearChip.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-chip
+      v-for="participant in participants"
+      :key="participant.year"
+      color="blue lighten-4"
+      class="font-weight-medium mr-3"
+      label
+      small>
+      <span class="font-weight-medium">{{participant.year}}</span>
+    </v-chip>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    participants: {type: Array, required: true}
+  }
+}
+</script>
